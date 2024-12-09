d3269a8daebc0334408d13f634283e9e69e3513a
KryvavyiPotii <144026226+KryvavyiPotii@users.noreply.github.com>
2024-12-09 22:34:07 +0100

    Update birthdays.rs
    * Constant values are now passed as attack parameters

--- src/hashattacks/birthdays.rs
+++ src/hashattacks/birthdays.rs
@@ -1,100 +1,111 @@
 use std::sync::{Arc, atomic::{AtomicBool, Ordering}};
 
+use crate::messagehash::HashValue;
+
 use super::{AttackResult, AttackState, HashAttack, MessageHash};
-
-
-const NUMBER_OF_VERBOSE_TRIES: u64 = 30;
-const PROBABILITY: f32 = 0.95;
-const TRUNCATED_HASH_SIZE_IN_BITS: usize = 32;
-const TRUNCATED_HASH_SIZE_IN_BYTES: usize = TRUNCATED_HASH_SIZE_IN_BITS / 8;
-const HASH_COUNT: u64 = 1 << TRUNCATED_HASH_SIZE_IN_BITS;
-
-
-fn min_probability_from_tries(tries: u64) -> f32 {
-    1.0 + (-(tries as f32) / HASH_COUNT as f32).exp()
-}
-
-fn tries_from_probability(probability: f32) -> u64 {
-    (HASH_COUNT as f32 * (1.0 / (1.0 - probability)).ln()).ceil()
-        as u64
-}
-
-fn find_collision(
-    hashes: &mut Vec<MessageHash>,
-    prefix_len_in_bytes: usize
-) -> Option<(MessageHash, MessageHash)> {
-    let i = hashes.len();
-
-    if i <= 1 {
-        return None;
-    }
-
-    let messagehash1 = hashes.pop().unwrap();
-    let hash_len = messagehash1.hash_len();
-
-    let prefix_index = if hash_len >= prefix_len_in_bytes {
-        hash_len - prefix_len_in_bytes
-    }
-    else {
-        0
-    };
-
-    for (j, messagehash2) in hashes.iter().enumerate() {
-        let message1 = &messagehash1.message();
-        let hash1 = &messagehash1.hash_value()[prefix_index..];
-        let message2 = &messagehash2.message();
-        let hash2 = &messagehash2.hash_value()[prefix_index..]; 
-
-        if message1 != message2 && hash1 == hash2 {
-            println!(
-                "[SUCCESS] Found collision in iteration {}-{}!\n{}\n{}\n",
-                i,
-                j,
-                messagehash1,
-                messagehash2
-            );
-
-            return Some((messagehash1, messagehash2.clone()));
-        }
-    }
-
-    hashes.push(messagehash1);
-
-    None
-}
 
 
 pub struct Birthdays {
     state: AttackState,
+    hash_size_in_bytes: usize,
+    success_probability: f32,
+    verbose_tries_number: u64
 }
 
 impl Birthdays {
-    pub fn new(initial_state: AttackState) -> Self {
-        Self { state: initial_state }
+    pub fn build(
+        initial_state: AttackState,
+        hash_size_in_bytes: usize,
+        success_probability: f32,
+        verbose_tries_number: u64
+    ) -> Result<Self, &'static str> {
+        if hash_size_in_bytes > HashValue::len() {
+            return Err("Invalid hash size");
+        }
+
+        Ok(
+            Self { 
+                state: initial_state,
+                hash_size_in_bytes,
+                success_probability,
+                verbose_tries_number
+            }
+        )
+    }
+
+    fn min_probability_from_tries(&self, tries: u64) -> f32 {
+        let hash_size_in_bits = self.hash_size_in_bytes * 8; 
+        let hash_count = 1 << hash_size_in_bits;
+
+        1.0 + (-(tries.pow(2) as f32) / (2 * hash_count) as f32).exp()
+    }
+
+    fn tries_from_probability(&self) -> u64 {
+        let hash_size_in_bits = self.hash_size_in_bytes * 8; 
+        let hash_count: u64 = 1 << hash_size_in_bits;
+        let probability = self.success_probability;
+
+        let count_part = (2.0 * hash_count as f64).sqrt(); 
+        let ln_part = ((1.0 / (1.0 - probability)).ln() as f64).sqrt();
+        
+        (count_part * ln_part).ceil() as u64
+    }
+
+    fn find_collision(
+        &self,
+        hashes: &mut Vec<MessageHash>
+    ) -> Option<(MessageHash, MessageHash)> {
+        let i = hashes.len();
+
+        if i <= 1 {
+            return None;
+        }
+
+        let messagehash1 = hashes.pop().unwrap();
+        
+        for (j, messagehash2) in hashes.iter().enumerate() {
+            if messagehash1.collides_with(
+                messagehash2,
+                self.hash_size_in_bytes
+            ) {
+                println!(
+                    "[SUCCESS] Found collision in iteration {}-{}!\n{}\n{}\n",
+                    i,
+                    j,
+                    messagehash1,
+                    messagehash2
+                );
+
+                return Some((messagehash1, messagehash2.clone()));
+            }
+        }
+
+        hashes.push(messagehash1);
+
+        None
     }
 }
 
 impl HashAttack for Birthdays {
     fn attack(&mut self, running: Arc<AtomicBool>) -> AttackResult {
-        println!("Initialising birthday attack...\n{}\n",
+        println!("[INFO] Initialising birthday attack...\n{}\n",
             self.state.messagehash()
         );
 
-        println!("Searching for a collision...");
+        println!("[INFO] Searching for a collision...");
         
         let mut i = 1;
         let mut calculated_hashes = Vec::new();
 
-        while i <= NUMBER_OF_VERBOSE_TRIES && running.load(Ordering::SeqCst) {
+        while i <= self.verbose_tries_number && running.load(Ordering::SeqCst) {
             let messagehash = self.state.update();
             
             println!("{}\t{}", i, messagehash);        
             
             calculated_hashes.push(messagehash);
             
-            if let Some((mh1, mh2)) = find_collision(
-                &mut calculated_hashes,
-                TRUNCATED_HASH_SIZE_IN_BITS
+            if let Some((mh1, mh2)) = self.find_collision(
+                &mut calculated_hashes
             ) {
                 return AttackResult::Collision(mh1.message(), mh2.message());
             }
@@ -104,16 +115,15 @@ impl HashAttack for Birthdays {
 
         println!("...\n");
 
-        let tries_num = tries_from_probability(PROBABILITY);
+        let tries_num = self.tries_from_probability();
         
         while i <= tries_num && running.load(Ordering::SeqCst) {
             let messagehash = self.state.update();
 
             calculated_hashes.push(messagehash);
             
-            if let Some((mh1, mh2)) = find_collision(
-                &mut calculated_hashes,
-                TRUNCATED_HASH_SIZE_IN_BITS
+            if let Some((mh1, mh2)) = self.find_collision(
+                &mut calculated_hashes
             ) {
                 return AttackResult::Collision(mh1.message(), mh2.message());
             }
